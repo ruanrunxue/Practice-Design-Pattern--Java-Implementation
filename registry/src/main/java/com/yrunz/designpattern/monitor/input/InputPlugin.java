a35223a41a1bb1d37bcf8f7b923e3f14c8e5d796
元闰子 <runxueruan@gmail.com>
2022-02-12 20:15:54 +0800

    add 策略模式；input plugin不同的策略实现

--- registry/src/main/java/com/yrunz/designpattern/monitor/input/InputPlugin.java
+++ registry/src/main/java/com/yrunz/designpattern/monitor/input/InputPlugin.java
@@ -4,6 +4,10 @@ import com.yrunz.designpattern.monitor.config.Config;
 import com.yrunz.designpattern.monitor.plugin.Event;
 import com.yrunz.designpattern.monitor.plugin.Plugin;
 
+/**
+ * 策略模式
+ */
+
 public interface InputPlugin extends Plugin {
     Event input();
     void setContext(Config.Context context);
