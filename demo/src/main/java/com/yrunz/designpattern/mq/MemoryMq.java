d6c77183806a414f892601654df153e36b0c9d05
元闰子 <runxueruan@gmail.com>
2022-02-16 12:48:41 +0800

    新增Mq接口，MemoryMq实现Mq接口

--- demo/src/main/java/com/yrunz/designpattern/mq/MemoryMq.java
+++ demo/src/main/java/com/yrunz/designpattern/mq/MemoryMq.java
@@ -8,7 +8,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 // 懒汉式单例模式，双重校验锁
 // 内存消息队列，使用Message作为通信载体
 // 队列内划分多个topic，使用者必须先创建topic后再进行通信
-public class MemoryMq {
+public class MemoryMq implements Mq {
     private volatile static MemoryMq INSTANCE = null;
     // key为Topic
     private final Map<String, Queue<Message>> queues;
@@ -28,6 +28,7 @@ public class MemoryMq {
         return INSTANCE;
     }
 
+    @Override
     public void createTopic(String topic) {
         if (queues.containsKey(topic)) {
             throw new TopicAlreadyExistException(topic);
@@ -35,10 +36,12 @@ public class MemoryMq {
         queues.put(topic, new LinkedBlockingQueue<>());
     }
 
+    @Override
     public void deleteTopic(String topic) {
         queues.remove(topic);
     }
 
+    @Override
     public void produce(Message message) {
         if (!queues.containsKey(message.topic())) {
             throw new TopicNotFoundException(message.topic());
@@ -46,6 +49,7 @@ public class MemoryMq {
         queues.get(message.topic()).offer(message);
     }
 
+    @Override
     public Message consume(String topic) {
         if (!queues.containsKey(topic)) {
             throw new TopicNotFoundException(topic);
---
+++ demo/src/main/java/com/yrunz/designpattern/mq/Mq.java
@@ -0,0 +1,12 @@
+package com.yrunz.designpattern.mq;
+
+// 消息队列抽象接口
+public interface Mq {
+    void createTopic(String topic);
+
+    void deleteTopic(String topic);
+
+    void produce(Message message);
+
+    Message consume(String topic);
+}
